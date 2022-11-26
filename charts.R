889a95f2ad69d4ac2430f6b0e157950af0826d2d
Rohan <33783348+selroh18@users.noreply.github.com>
2022-11-26 20:22:10 +0000

    add figures, update charts code

--- charts.R
+++ charts.R
@@ -6,31 +6,36 @@ library(scales)
 library(lubridate)
 library(ggrepel)
 library(stringr)
+library(tidyr)
 
 # Chart 1
 economics <- read.xlsx('data.xlsx', sheet='economics')
 economics %>%
+  mutate(return=return / 100, err = err / 100) %>%
   ggplot(aes(reorder(subject, desc(return)), return, ymin=return-err, ymax=return+err, colour=group)) +
   geom_pointrange() +
   scale_colour_discrete(breaks=c('STEM', 'LEM', 'Other')) +
   scale_y_continuous(labels = percent) +
-  theme_economist() +
+  theme_bw() +
+  # theme_economist() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1),
-        legend.title=element_blank()) +
-  labs(x='Subject', y='Relative return', title='Figure 1: Relative returns of subjects')
+        legend.position=c(.9, 0.82)) +
+  labs(x='Subject', y='Relative return', colour='Subject group',
+       title='Figure 1: Returns of degree subjects relative to History')
 
 # Chart 2
 admissions <- read.xlsx('data.xlsx', sheet='admissions') %>%
   mutate(year=as.Date(ISOdate(year, 1, 1)))
 start <- min(admissions$year)
 end <- max(admissions$year)
+top <- max(admissions$total)
 
 admissions %>%
   ggplot(aes(year, total)) +
-  geom_line() +
+  geom_area() +
   scale_x_date(date_breaks='10 year', date_labels='%Y', limits=c(start, end), expand=c(0, 0)) +
-  scale_y_continuous(labels = comma) +
-  theme_economist() +
+  scale_y_continuous(labels = comma, expand = c(0,0)) +
+  theme_bw() +
   theme(axis.text.y=element_text(hjust=0.95),
     plot.margin=margin(10, 20, 10, 10)) +
   labs(x='Year', y='Number of students entering first degree', title='Figure 2: Higher education admissions numbers')
@@ -46,20 +51,25 @@ mobility %>%
             vjust="inward", hjust="inward") +
   scale_y_continuous(labels = percent) +
   scale_x_continuous(labels = percent) +
-  guides(color = guide_legend(nrow = 3), text='none') +
-  theme_economist() +
-  labs(x='Access rate', y='Success rate', colour='University type', title='Figure 3: Social mobility by higher education institution')
+  scale_colour_discrete(breaks=c('Most Selective Russell', 'Other Russell Group', 'Old Universities (Pre-1992)',
+                                 'Post-1992 (more selective)', 'Post-1992 (least selective)')) +
+  guides(color = guide_legend(nrow = 5), text='none') +
+  theme_bw() +
+  theme(legend.position=c(0.85, 0.8)) +
+  labs(x='Access rate', y='Success rate', colour='University type',
+       title='Figure 3: Social mobility by higher education institution')
 
 # Chart 4
 exams <- read.xlsx('data.xlsx', sheet='exams')
 exams %>%
   ggplot(aes(reorder(subject, desc(accuracy)), accuracy)) +
-  geom_point() +
+  geom_point(size=3) +
   scale_y_continuous(labels = percent, limits=c(0, 1)) +
-  theme_economist() +
+  theme_bw() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1),
         legend.title=element_blank()) +
-  labs(x='Subject', y='Probability of definitive grade', title='Figure 4: Qualification accuracy')
+  labs(x='Subject', y='Probability of definitive grade',
+       title='Figure 4: Qualification accuracy by subject')
 
 # Chart 5
 curriculum <- read.xlsx('data.xlsx', sheet='curriculum') %>%
@@ -73,6 +83,7 @@ curriculum %>%
   geom_area() +
   scale_x_date() +
   scale_y_continuous(labels = comma) +
-  theme_economist() +
-  theme() +
-  labs(x='Year', y='Number of candidates', title='Figure 5: AS- and A-level candidates by subject')
+  theme_bw() +
+  theme(legend.position=c(0.85, 0.84)) +
+  labs(x='Year', y='Number of candidates', fill='Subject group',
+       title='Figure 5: AS- and A-level candidates by subject')
